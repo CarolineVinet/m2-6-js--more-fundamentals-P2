7953808666114bd04a9fb1b49f11c958248c48d1
Caroline Vinet <carovinet@gmail.com>
2020-06-23 17:26:20 -0400

    exercise7

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -2,15 +2,23 @@
 // ----------
 
 let foodPairings = {
-  burgers: 'beer',
-  fish: 'white wine',
-  beef: 'red wine',
-  strawberries: 'cream',
+  burgers: "beer",
+  fish: "white wine",
+  beef: "red wine",
+  strawberries: "cream",
 };
 
-// createRecommendations function
+function createRecommendations(obj) {
+  const allKeys = Object.keys(obj);
+  const newArr = allKeys.map(function (key) {
+    return `With ${key} it is best to have ${obj[key]}.`;
+  });
+  return newArr;
+}
 
-// printRecommendations function
-
-// function call (done)
+function printRecommendations(arr) {
+  arr.forEach(function (string) {
+    console.log(string);
+  });
+}
 printRecommendations(createRecommendations(foodPairings));
