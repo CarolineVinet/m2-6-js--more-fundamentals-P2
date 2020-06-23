fb63a0c46d72e51450760ae3064b24b92f8d3388
Caroline Vinet <carovinet@gmail.com>
2020-06-23 14:57:03 -0400

    exercise3

--- workshop/exercise-3.js
+++ workshop/exercise-3.js
@@ -2,14 +2,24 @@
 // ----------
 
 let mostPopularFood = [
-  'bean burritos',
-  'poke bowl',
-  'chicken sliders',
-  'pork baby back ribs',
-  'chicken burritos',
-  'chicken sandwich',
-  'cauliflower rice bowl',
-  'chicken and waffle sliders',
-  'parmesan chicken',
-  'buffalo-flavored cauliflower',
+  "bean burritos",
+  "poke bowl",
+  "chicken sliders",
+  "pork baby back ribs",
+  "chicken burritos",
+  "chicken sandwich",
+  "cauliflower rice bowl",
+  "chicken and waffle sliders",
+  "parmesan chicken",
+  "buffalo-flavored cauliflower",
 ];
+
+const sortList = (arr) => {
+  console.log(arr);
+  let copiedArray = [...arr];
+  let alphOrder = copiedArray.sort();
+  for (let i = 0; i < alphOrder.length; i++) {
+    console.log(`${alphOrder[i]} (${arr.indexOf(alphOrder[i]) + 1})`);
+  }
+};
+sortList(mostPopularFood);
