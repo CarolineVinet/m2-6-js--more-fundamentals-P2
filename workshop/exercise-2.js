71c3340f049b6a61f3724af8075697ebff90df92
Caroline Vinet <carovinet@gmail.com>
2020-06-23 13:30:02 -0400

    exercise2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -1,5 +1,12 @@
 // Exercise 2
 // ----------
 
-const lateComers = ['Felicia', 'Charles', 'Terry'];
-const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+const lateComers = ["Felicia", "Charles", "Terry"];
+const earlyBirds = ["Ellen", "Jerry", "Jasmine"];
+
+const printGuestOrder = (early, late) => {
+  let newArr = early.concat(late);
+  console.log(newArr);
+};
+
+printGuestOrder(earlyBirds, lateComers);
